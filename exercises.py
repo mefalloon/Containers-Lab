1f720952bbd4f104c0eb1e23475b72177098baad
elizafalloon-ga <elizafalloon@gmail.com>
2022-04-20 16:22:25 -0700

    Ex 1-3

--- exercises.py
+++ exercises.py
@@ -3,15 +3,23 @@
 # Print out the second student's name.
 # Print out the last student's name.
 
-
+# students = ['Kanye', 'Kim_K', 'Pete']
+#     print(students[1])
+#     print(students[2])
 # Exercise 2
 # Create a tuple named foods containing the same number of foods (strings) as there are names in the students list.
 # Use a for loop to print out the string "food goes here is a good food".
 
+# foods = ('oxtail', 'donut', 'tacos')
 
+# for food in foods:
+#     # print(f"{food} is a good food ")  
 # Exercise 3
 # Using a for loop, print just the last two food strings from foods.
+foods = ('oxtail', 'donut', 'tacos')
 
+for food in foods[1:]:
+    print(food)
 
 # Exercise 4
 # Create a dictionary named home_town containing the keys of city, state and population.
