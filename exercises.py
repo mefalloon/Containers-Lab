8e6147baca3a49500648d848aaf0f0787fa217f4
elizafalloon-ga <elizafalloon@gmail.com>
2022-04-20 16:46:18 -0700

    print string format

--- exercises.py
+++ exercises.py
@@ -6,14 +6,17 @@
 # students = ['Kanye', 'Kim_K', 'Pete']
 #     print(students[1])
 #     print(students[2])
+
 # Exercise 2
 # Create a tuple named foods containing the same number of foods (strings) as there are names in the students list.
 # Use a for loop to print out the string "food goes here is a good food".
 
-# foods = ('oxtail', 'donut', 'tacos')
+foods = ('oxtail', 'donut', 'tacos')
 
-# for food in foods:
-#     # print(f"{food} is a good food ")  
+for food in foods:
+
+    print(f"{food} is a good food ")  
+
 # Exercise 3
 # Using a for loop, print just the last two food strings from foods.
 foods = ('oxtail', 'donut', 'tacos')
@@ -25,6 +28,14 @@ for food in foods[1:]:
 # Create a dictionary named home_town containing the keys of city, state and population.
 # Print a string with this format:
 # "I was born in city, state - population of population"
+
+home_town = {
+    'city': 'Miami',
+    'state': 'Florida',
+    'population': 2
+}
+
+print(f"I was born in {home_town['city']}, {home_town['state']} - population of {home_town['population']}")
 
 
 # Exercise 5
