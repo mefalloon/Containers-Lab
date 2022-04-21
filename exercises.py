73c3628b1b3d9c1f5bc94fe48014c2d6744a707d
elizafalloon-ga <elizafalloon@gmail.com>
2022-04-20 21:58:20 -0700

    unsaved-refinalized

--- exercises.py
+++ exercises.py
@@ -4,8 +4,8 @@
 # Print out the last student's name.
 
 students = ['Kanye', 'Kim_K', 'Pete']
-#     print(students[1])
-#     print(students[2])
+    print(students[1])
+    print(students[2])
 
 # Exercise 2
 # Create a tuple named foods containing the same number of foods (strings) as there are names in the students list.
@@ -13,29 +13,29 @@ students = ['Kanye', 'Kim_K', 'Pete']
 
 foods = ('oxtail', 'donut', 'tacos')
 
-# for food in foods:
+for food in foods:
 
-#     print(f"{food} is a good food ")  
+    print(f"{food} is a good food ")  
 
 # # Exercise 3
 # # Using a for loop, print just the last two food strings from foods.
-# foods = ('oxtail', 'donut', 'tacos')
+foods = ('oxtail', 'donut', 'tacos')
 
-# for food in foods[1:]:
-#     print(food)
+for food in foods[1:]:
+    print(food)
 
 # # Exercise 4
 # # Create a dictionary named home_town containing the keys of city, state and population.
 # # Print a string with this format:
 # # "I was born in city, state - population of population"
 
-# home_town = {
-#     'city': 'Miami',
-#     'state': 'Florida',
-#     'population': 2
-# }
+home_town = {
+    'city': 'Miami',
+    'state': 'Florida',
+    'population': 2
+}
 
-# print(f"I was born in {home_town['city']}, {home_town['state']} - population of {home_town['population']}")
+print(f"I was born in {home_town['city']}, {home_town['state']} - population of {home_town['population']}")
 
 
 # # Exercise 5
@@ -44,8 +44,8 @@ foods = ('oxtail', 'donut', 'tacos')
 # # "state = California"
 # # "population = 58000"
 
-# for key, val in home_town.items():
-#   print( f"{key} = {val}" )
+for key, val in home_town.items():
+  print( f"{key} = {val}" )
 
 
 
@@ -57,29 +57,30 @@ foods = ('oxtail', 'donut', 'tacos')
 # #  enumerate  --This lets you get the index of an element while iterating over a list
 
 
-# for index, student in enumerate(students):
-#   cohort.append({
-#     'student': student,
-#     'fav_food': foods[index]
-#   })
+for index, student in enumerate(students):
+  cohort.append({
+    'student': student,
+    'fav_food': foods[index]
+  })
 
-# #    'student': 'Tina',
-# #    'fav_food' 'Cheeseburger'
-# #  }
-# # Iterate over cohort printing out each element.
-# for student in cohort:
-#   print(student)
+#    'student': 'Tina',
+#    'fav_food' 'Cheeseburger'
+#  }
+# Iterate over cohort printing out each element.
+for student in cohort:
+  print(student)
 
 # # Exercise 7
 # # Using the list of students and list comprehension, assign to a variable named awesome_students a new list containing strings similar to this:
 # # ["Tina is awesome!", "Fred is awesome!", "Wilma is awesome!"]
 # # Iterate over awesome_students printing out each string.
 
-# awesome_students = [f"{student} is awesome" for student in students]
-# print(awesome_students)
+awesome_students = [f"{student} is awesome" for student in students]
+print(awesome_students)
 
 
 # Exercise 8
 # Using the tuple foods and list comprehension within a for loop, print each food string that contains the letter a.
+# how to say I want (expression) foreach food in the list 
 find_a = [food for food in foods if 'a' in food]
 print(find_a)
